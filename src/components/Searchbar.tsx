556a10afb463c22c7181d8ea06bdaaa08925a492
vishaldethe15 <dethevishal9@gmail.com>
2025-04-08 16:16:04 +0530

    search functionality

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,22 +1,29 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
+import { useDebounce } from "@/hooks/useDebounce";
 
 const SearchBar = () => {
-  const [search, setSearch] = useState("");
+  const searchParam = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
+  const handleSearch = useDebounce((e) => {
+    const searchQuery = e.target.value;
+    const params = new URLSearchParams(searchParam);
+    searchQuery.length >= 3
+      ? params.set("query", searchQuery)
+      : params.delete("query");
+    replace(`${pathname}?${params}`);
+  }, 300);
 
   return (
-    <div className="w-full max-w-sm px-4 py-2 flex items-center gap-4 rounded-2xl border-2 border-neutral-900 text-neutral-800 ">
+    <div className="bg-neutral-200 max-w-xs mx-auto md:mx-0 px-4 py-2 flex items-center gap-4 rounded-4xl text-neutral-800 ">
       <Image src={"/pokeicon.png"} alt="poke icon" width={20} height={20} />
       <input
         type="text"
-        placeholder="Search Pokémon by Name or ID..."
-        value={search}
-        onChange={handleChange}
+        placeholder="Search Pokémon by Name..."
+        onChange={handleSearch}
         className="w-full focus:outline-none "
       />
     </div>
