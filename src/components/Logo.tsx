3542d639190346c3c150468ff59a35888f6faf74
vishaldethe15 <dethevishal9@gmail.com>
2025-04-08 16:15:40 +0530

    add link

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
   return (
     <div className="hidden sm:flex items-center">
-      <Image src="/pokelogo.png" alt="Pokemon Logo" width={150} height={150} />
+      <Link href={"/"}>
+        <Image
+          src="/pokelogo.png"
+          alt="Pokemon Logo"
+          width={150}
+          height={150}
+        />
+      </Link>
     </div>
   );
 };
